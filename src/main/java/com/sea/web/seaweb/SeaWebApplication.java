cb4f432ac8e3b2e2db1f78caec505d249d538f51
Claudiomar Araujo <claudiomarpda@gmail.com>
2018-05-19 17:15:21 -0300

    Spring Initializr: Web, Thymeleaf, Security, JPA and MySQL

---
+++ src/main/java/com/sea/web/seaweb/SeaWebApplication.java
@@ -0,0 +1,12 @@
+package com.sea.web.seaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeaWebApplication.class, args);
+    }
+}
